7e2d1a72a38d1266434b9c87c73114adc73643b5
Abel Georges <abelgr8@gmail.comgit config --global user.name Abel>
2018-03-19 19:05:17 -0400

    :)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,74 +1,74 @@
 var words = ["hawks", "celtics", "nets", "hornets", "bulls", "cavaliers", "mavericks", "nuggets", "pistons", "warriors", "rockets", "pacers", "clippers", "lakers", "grizzlies", "heat", "bucks", "timberwolves", "pelicans", "knicks", "thunder", "magic", "76ers", "suns", "trail blazers", "kings", "spurs", "raptors", "jazz", "wizards",
 ];
 
-var selectedWord = "";
+var randomWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
-var blanksAndSuccesses = [];
+var letterReveal = [];
 var wrongLetters = [];
 
 var winCount = 0;
 var lossCount = 0;
-var guessesLeft = 6;
+var chancesLeft = 6;
 
 function startGame () {
-    selectedWord = words[Math.floor(Math.random() * words.length)];
-    lettersinWord = selectedWord.split("");
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    lettersinWord = randomWord.split("");
     numBlanks = lettersinWord.length;
 
     //reset
-    guessesLeft = 6;
+    chancesLeft = 6;
     wrongLetters = [];
-    blanksAndSuccesses = [];
+    letterReveal = [];
 
     // black slides
     for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("_"); 
+        letterReveal.push("_"); 
     }
 
     // Writing to DOM
-    document.getElementById("game").innerHTML = blanksAndSuccesses.join(" ");
-    document.getElementById("chances").innerHTML = guessesLeft;
+    document.getElementById("game").innerHTML = letterReveal.join(" ");
+    document.getElementById("chances").innerHTML = chancesLeft;
     document.getElementById("wins").innerHTML = winCount;
     document.getElementById("losses").innerHTML = lossCount;
 
     //test
-    console.log(selectedWord);
+    console.log(randomWord);
     console.log(lettersinWord)
     console.log(numBlanks);
-    console.log(blanksAndSuccesses);
+    console.log(letterReveal);
    
     //User is guessing
     document.onkeyup = function(event) {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        if (selectedWord.indexOf(userGuess) > -1 ) {
-            for(var i = 0; i < selectedWord.length; i++) {
-                if (selectedWord[i] === userGuess) {
-                    blanksAndSuccesses[i] = userGuess;
-                    document.getElementById("game").innerHTML = blanksAndSuccesses.join(" ");
+        if (randomWord.indexOf(userGuess) > -1 ) {
+            for(var i = 0; i < randomWord.length; i++) {
+                if (randomWord[i] === userGuess) {
+                    letterReveal[i] = userGuess;
+                    document.getElementById("game").innerHTML = letterReveal.join(" ");
                     //test
-                    console.log(blanksAndSuccesses);
+                    console.log(letterReveal);
                 }
             }
         } else {
             $("#wrong").append("  " + userGuess + "  ");
-            document.getElementById("chances").innerHTML = guessesLeft;
-            guessesLeft--;
+            document.getElementById("chances").innerHTML = chancesLeft;
+            chancesLeft--;
         }
         
     }
 }
 
 function userWorL () {
-    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left: " + guessesLeft);
+    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left: " + chancesLeft);
     
     // win or lose
-    if (lettersinWord.toString() === blanksAndSuccesses.toString()) {
+    if (lettersinWord.toString() === letterReveal.toString()) {
         winCount++;
         alert ("You Win");
         document.getElementById("wins").innerHTML = winCount;
         startGame();
-    } else if (guessesLeft == 0) {
+    } else if (chancesLeft == 0) {
         lossCount++;
         alert ("You Lost");
         document.getElementById("losses").innerHTML = lossCount;
